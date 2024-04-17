0e3346437c435aa0f8892909747153ba27386d3d
kadir-zeren <k4dzeren@gmail.com>
2024-04-17 20:01:40 +0300

    - operatoru

---
+++ operators.js
@@ -0,0 +1,51 @@
+console.log("Js dosyasi baglandi");
+
+var deneme = 5;
+vardeneme = 6;
+
+let myVar = 5;
+
+const myVar1 = 6;
+
+const num1 = 10;
+const num2 = 69;
+
+const toplam = num1 + num2;
+
+console.log(toplam);
+
+const msg1 = "Hello";
+const msg2 = "World";
+
+const mesaj = msg1 + msg2;
+console.log(mesaj);
+
+const firstName = "Koray";
+const lastName = "Koksal";
+console.log(firstName, lastName);
+
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+
+const fullName2 = `${firstName} ${lastName}`;
+console.log(fullName2);
+
+const num3 = "6";
+const num4 = "7";
+console.log(num3 + num4);
+console.log(num3 + 7);
+
+console.log(7 + num3);
+
+console.log(typeof (7 + num3));
+
+const x = 55;
+const y = 44;
+
+console.log(x - y);
+
+const myNum = "1";
+console.log(x - myNum);
+
+const yourNum = "one";
+console.log(x - yourNum);
